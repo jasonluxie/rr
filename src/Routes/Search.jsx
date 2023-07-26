4fb2176032678771b0855b73b7e39b7cd45bbdb9
Jason Xie <jasonluxie@gmail.com>
2023-07-26 13:01:59 -0500

    routing

---
+++ src/Routes/Search.jsx
@@ -0,0 +1,12 @@
+const Search = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            {/* <input id="api_key" type="text" size="50"/> */}
+            <input id="searchTextField" type="text" size="50" />
+        </div>
+    );
+}
+
+
+export default Search
