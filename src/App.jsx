fa288239f45babb1476d412c0bfa1bfa85f81a08
Jason Xie <jasonluxie@gmail.com>
2023-07-26 21:52:22 -0500

    autocomplete functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import { Loader } from "@googlemaps/js-api-loader";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Search from "./Routes/Search";
 // import Result from "./Routes/Result";
 
 function App() {
     const [count, setCount] = useState(0);
-
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Search/>} />
+                    <Route path="/" element={<Search />} />
                     {/* <Route path="/result" element={Result} /> */}
                 </Routes>
             </BrowserRouter>
--- src/Routes/Search.jsx
+++ src/Routes/Search.jsx
@@ -1,12 +1,36 @@
+import { useRef, useEffect } from "react";
 const Search = () => {
+    const autoCompleteRef = useRef();
+    const inputRef = useRef();
+    const options = {
+        fields: ["address_components", "name", "geometry"],
+        types: ["restaurant"],
+        componentRestrictions: { country: ["US", "PH", "JP", "MX"] },
+    };
+    useEffect(() => {
+        autoCompleteRef.current = new google.maps.places.Autocomplete(
+            inputRef.current,
+            options
+        );
+    }, []);
     return (
         <div>
-            <h1>Hello World!</h1>
-            {/* <input id="api_key" type="text" size="50"/> */}
-            <input id="searchTextField" type="text" size="50" />
+<script
+    async
+    src={`https://maps.googleapis.com/maps/api/js?key=${
+        import.meta.env.VITE_MAPS_API
+    }&libraries=places&callback=initMap`}
+></script>
+            <h1>Restaurant Royale</h1>
+            <input
+                ref={inputRef}
+                id="searchTextField"
+                type="text"
+                size="50"
+                placeholder="Enter your address"
+            />
         </div>
     );
-}
+};
 
-
-export default Search
+export default Search;
