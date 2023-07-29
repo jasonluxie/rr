e6df0c87b8837fd25d998fa9f3c7cf6a74979141
Jason Xie <jasonluxie@gmail.com>
2023-07-29 00:15:17 -0500

    moving to cloudflare workers

--- src/Routes/Search.jsx
+++ src/Routes/Search.jsx
@@ -1,26 +1,9 @@
 import { useRef, useEffect } from "react";
+
 const Search = () => {
-    const autoCompleteRef = useRef();
-    const inputRef = useRef();
-    const options = {
-        fields: ["address_components", "name", "geometry"],
-        types: ["restaurant"],
-        componentRestrictions: { country: ["US", "PH", "JP", "MX"] },
-    };
-    useEffect(() => {
-        autoCompleteRef.current = new google.maps.places.Autocomplete(
-            inputRef.current,
-            options
-        );
-    }, []);
+    
     return (
         <div>
-<script
-    async
-    src={`https://maps.googleapis.com/maps/api/js?key=${
-        import.meta.env.VITE_MAPS_API
-    }&libraries=places&callback=initMap`}
-></script>
             <h1>Restaurant Royale</h1>
             <input
                 ref={inputRef}
